a3c0ca0b1c507bf259237e7268478e6c337a2482
tangzhushou <ellison.tang@gmail.com>
2022-02-09 13:23:57 +0800

    multiple devices over both network and usb are supported.

--- case_study/huaweiAI/setTemplateFace.205.2.py
+++ case_study/huaweiAI/setTemplateFace.205.2.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2022/1/24 15:14
 # @Author  : fy
-# @FileName: setTemplateFace.py
+# @FileName: setTemplateFace.205.2.py
 
 import subprocess
 
@@ -17,14 +17,19 @@ log = logging.getLogger(__name__)
 
 global g_script
 
-g_config = {
+g_isNetwork = False
+g_dev_serial = "192.168.2.205:5555" if g_isNetwork else "JAM6R20406000098"
+# g_remote_frida_dev_serial = '192.168.2.197:27042'
+
+
+g_fuzz_config = {
     "g_mem_block": 2,
-    "g_mem_offset": 496208,
+    "g_mem_offset": 563896,
     "g_log_file": "setTemplateFace_2_crash.log",
     "g_model_size": 0x1d5b50,
 
     # "g_mem_block": 1,
-    # "g_mem_offset": 557912,
+    # "g_mem_offset": 563784,
     # "g_log_file": "setTemplateFace_1_0x102536_crash.log",
     # "g_model_size": 0x102536,
 
@@ -40,7 +45,7 @@ g_config = {
 def collect_crash_log():
     retMe = False
     print("|-[d] collect_crash_log_1")
-    p = subprocess.Popen("adb logcat -b crash -d",
+    p = subprocess.Popen("adb -s {} logcat -b crash -d".format(g_dev_serial),
                          shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE
                          )
@@ -48,11 +53,11 @@ def collect_crash_log():
 
     print("|-[d] collect_crash_log_2")
     if "Build fingerprint" in stdout.decode():
-        with open(g_config["g_log_file"], "a+") as fwh:
+        with open(g_fuzz_config["g_log_file"], "a+") as fwh:
             fwh.write(stdout.decode())
             fwh.write("\n")
             retMe = True
-    p = subprocess.Popen("adb logcat -c",
+    p = subprocess.Popen("adb -s {} logcat -c".format(g_dev_serial),
                          shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE
                          )
@@ -62,7 +67,7 @@ def collect_crash_log():
     return retMe
 
 def on_message(message, data):
-    global g_config
+    global g_fuzz_config
 
     if message["type"] == "send":
         if "info" in message["payload"]:
@@ -73,23 +78,23 @@ def on_message(message, data):
             timer.start();
             '''
             msg = message["payload"].strip().split("|")
-            g_config["g_mem_block"] = int(msg[1])
-            g_config["g_mem_offset"] = int(msg[2])
-            g_config["g_seed_index"] = int(msg[3])
+            g_fuzz_config["g_mem_block"] = int(msg[1])
+            g_fuzz_config["g_mem_offset"] = int(msg[2])
+            g_fuzz_config["g_seed_index"] = int(msg[3])
             collect_crash_log()
             log.info("[Host MSG]: fuzzing block: #{}, offset: {}, progress: {:.3%}".format(
-                g_config["g_mem_block"], g_config["g_mem_offset"], g_config["g_mem_offset"] / g_config["g_model_size"])
+                g_fuzz_config["g_mem_block"], g_fuzz_config["g_mem_offset"], float(g_fuzz_config["g_mem_offset"]) / g_fuzz_config["g_model_size"])
             )
             # if collect_crash_log():
-            #     g_config["g_mem_offset"] += 4
+            #     g_fuzz_config["g_mem_offset"] += 4
             #    new_round()
             print("|-[d] on_message_1")
             g_script.post({'type': 'synchronize',
-                               'payload': {"g_mem_block": g_config["g_mem_block"], "g_mem_offset": g_config["g_mem_offset"]}})
+                               'payload': {"g_mem_block": g_fuzz_config["g_mem_block"], "g_mem_offset": g_fuzz_config["g_mem_offset"]}})
             print("|-[d] on_message_2")
 
         if "error" in message["payload"]:
-            with open(g_config["g_log_file"], "a+") as fwh:
+            with open(g_fuzz_config["g_log_file"], "a+") as fwh:
                 fwh.write("---------- {} ----------\n".format(message["payload"]));
             print("|-[d] on_message_3")
             collect_crash_log()
@@ -97,16 +102,16 @@ def on_message(message, data):
 
             msg = message["payload"].strip().split("|")
             # the ExceptionHandler of client will be triggered multiple time.
-            if g_config["g_mem_offset"] > int(msg[2]):
+            if g_fuzz_config["g_mem_offset"] > int(msg[2]):
                 log.info("[Host MSG]: duplicate on_message enter")
                 return
 
-            g_config["g_mem_block"] = int(msg[1])
-            g_config["g_mem_offset"] = int(msg[2])
-            g_config["g_seed_index"] = int(msg[3])
-            log.info("[Host MSG]: gotcha err in block: #{}, offset: {}".format(g_config["g_mem_block"], g_config["g_mem_offset"]))
+            g_fuzz_config["g_mem_block"] = int(msg[1])
+            g_fuzz_config["g_mem_offset"] = int(msg[2])
+            g_fuzz_config["g_seed_index"] = int(msg[3])
+            log.info("[Host MSG]: gotcha err in block: #{}, offset: {}".format(g_fuzz_config["g_mem_block"], g_fuzz_config["g_mem_offset"]))
 
-            g_config["g_mem_offset"] += 4
+            g_fuzz_config["g_mem_offset"] += 4
             new_round()
             print("|-[d] on_message_5")
 
@@ -115,14 +120,17 @@ def new_round():
     # for app in frida.get_usb_device().enumerate_applications():
     #     print("[i] {}".format(app))
     # clean the env
-    p = subprocess.Popen("adb shell am force-stop {}".format("com.mlkit.sample.body"),
+    p = subprocess.Popen("adb -s {} shell am force-stop {}".format(g_dev_serial, "com.mlkit.sample.body"),
                          shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE
                          )
     stdout, stderr = p.communicate()
+    # if stderr != b"":
+    #     log.error(stderr)
+    #     return
 
     time.sleep(1)
-    p = subprocess.Popen("adb shell am start -n {}".format("com.mlkit.sample.body/com.huawei.mlkit.sample.activity.StartActivity"),
+    p = subprocess.Popen("adb -s {} shell am start -n {}".format(g_dev_serial, "com.mlkit.sample.body/com.huawei.mlkit.sample.activity.StartActivity"),
                          shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE
                          )
@@ -131,38 +139,36 @@ def new_round():
     while True:
         try:
             time.sleep(1)
-            frida.get_usb_device().get_process(g_config["g_proc_name"])
+            frida.get_device(g_dev_serial).get_process(g_fuzz_config["g_proc_name"])
             time.sleep(3)
             break
         except:
             pass
 
-    print("[*] new fuzzing starts from: {}/{}.".format(g_config["g_mem_offset"], g_config["g_mem_block"]))
+    print("[*] new fuzzing starts from: {}/{}.".format(g_fuzz_config["g_mem_offset"], g_fuzz_config["g_mem_block"]))
 
-    # usb session
-    # session = frida.get_usb_device().attach(g_config["g_proc_name"])
-
-    # remote session
+    # to enable remote session
     '''
-    1. config the `libgadget.config.so` file
+    1. config the `libgadget.config.so` file ?
     2. config adbd via usb connection:
     2.1. to enable adb over network: 
     adb kill-server
     adb tcpip 5555
-    adb connect 192.168.5.149:5555
+    adb connect 192.168.2.205:5555
+    adb connect 192.168.2.197:5555
     adb devices
     2.2 to disable adb over network:
     adb kill-server
     adb usb
     '''
-    session = frida.get_device_manager().add_remote_device('192.168.5.149:27042').attach(g_config["g_proc_name"])
+    session = frida.get_device(g_dev_serial).attach(g_fuzz_config["g_proc_name"])
 
     JSFile = open('setTemplateFace.js')
     JsCodeFromfile = JSFile.read()
-    JsCodeFromfile = JsCodeFromfile.replace("proc_name_AAoAA", g_config["g_proc_name"])
-    JsCodeFromfile = JsCodeFromfile.replace("mem_block_AAoAA", str(g_config["g_mem_block"]))
-    JsCodeFromfile = JsCodeFromfile.replace("mem_offset_AAoAA", str(g_config["g_mem_offset"]))
-    JsCodeFromfile = JsCodeFromfile.replace("model_size_AAoAA", str(g_config["g_model_size"]))
+    JsCodeFromfile = JsCodeFromfile.replace("proc_name_AAoAA", g_fuzz_config["g_proc_name"])
+    JsCodeFromfile = JsCodeFromfile.replace("mem_block_AAoAA", str(g_fuzz_config["g_mem_block"]))
+    JsCodeFromfile = JsCodeFromfile.replace("mem_offset_AAoAA", str(g_fuzz_config["g_mem_offset"]))
+    JsCodeFromfile = JsCodeFromfile.replace("model_size_AAoAA", str(g_fuzz_config["g_model_size"]))
     # print(JsCodeFromfile)
     global g_script
     g_script = session.create_script(JsCodeFromfile)
